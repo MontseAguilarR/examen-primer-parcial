1099b5ea8bcf309a0f9fe15fb43831cb32ee42a5
MontseAguilarR <89093129+MontseAguilarR@users.noreply.github.com>
2021-09-12 20:24:40 -0500

    Update Hotel.java

--- src/uaslp/enginering/exam/model/Hotel.java
+++ src/uaslp/enginering/exam/model/Hotel.java
@@ -1,14 +1,23 @@
 package uaslp.enginering.exam.model;
 
-import java.util.ArrayList;
+import uaslp.enginering.exam.model.Guest;
+import uaslp.enginering.exam.model.Reservation;
+import uaslp.enginering.exam.model.Room;
 
+import java.util.ArrayList;
 public class Hotel {
+    private String name; //nombre del hotel
+
     private ArrayList<Reservation> reservations;
 
 
-    public Hotel() {
+    public Hotel(String name) {
+        this.name=name;
         reservations = new ArrayList<>();
+        rooms=new ArrayList<>();////////////
     }
+
+
 
     public void reserveRoom(int roomNumber, Guest guest, String arrivalDate, int nights) {
         Reservation reservation = new Reservation();
@@ -21,7 +30,31 @@ public class Hotel {
         reservations.add(reservation);
     }
 
+
+
     public ArrayList<Reservation> getReservations() {
+
         return reservations;
     }
+
+
+
+
+////////////////////
+    public String getName() {
+        return name;
+    }
+    private ArrayList<Room> rooms;
+    public ArrayList<Room> getRooms() {
+
+        return rooms;
+    }
+
+    public void addRoom(Room room) {
+        room.getNumber();
+        room.getBed();
+        room.getStatus();
+        rooms.add(room);
+    }
 }
+
